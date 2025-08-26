3213cf0861a828aad587b6b4dd131b85f07539f0
dhruvalgondaliya <f342dhruval@gmail.com>
2025-08-26 18:31:15 +0530

    add role base access

--- src/Routes/Dashboard.js
+++ src/Routes/Dashboard.js
@@ -1,7 +1,11 @@
 import express from "express";
 import Auth from "../MiddleWare/Auth.js";
-import { getDashboardStats } from "../Controllers/Dashbord.js";
+import {
+  getDashboardStats,
+  getRestaurantStats,
+} from "../Controllers/Dashbord.js";
 
 export const Dashboard_Routes = express.Router();
 
 Dashboard_Routes.get("/stats", Auth, getDashboardStats);
+Dashboard_Routes.get("/getRestaurantData",Auth, getRestaurantStats);
